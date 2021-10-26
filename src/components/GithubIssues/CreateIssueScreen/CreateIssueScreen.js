705fce67403c9192f4d9f08f8dcde19ce736030b
Agustin <agustin.narvaez@contollo.net>
2021-10-26 15:50:48 -0600

    Implementing custom hook in create issue form

--- src/components/GithubIssues/CreateIssueScreen/CreateIssueScreen.js
+++ src/components/GithubIssues/CreateIssueScreen/CreateIssueScreen.js
@@ -1,21 +1,45 @@
+import { useForm } from "hooks/useForm";
 
 
 export const CreateIssueScreen = () => {
+
+  const [values, handleInputChange] = useForm({ title: '', comment: ''});
+
+  const { title, comment } = values;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <>
       <div className="container">
         <div class="card container mt-4">
           <div class="card-body">
-            
-            <div class="input-group mb-3">
-              <span class="input-group-text" id="basic-addon1"> Title </span>
-              <input type="text" class="form-control" placeholder="Title example" aria-label="Username" aria-describedby="basic-addon1" />
-            </div>
 
-            <div class="input-group">
-              <span class="input-group-text">Comment</span>
-              <textarea class="form-control" rows="5" aria-label="With textarea" placeholder="Leave a comment here!"></textarea>
-            </div>
+            <form onSubmit={handleSubmit}>
+            
+              <div class="input-group mb-3">
+                <span class="input-group-text" id="basic-addon1"> Title </span>
+                <input type="text" class="form-control" placeholder="Title example"
+                  name="title"
+                  value={title}
+                  onChange={handleInputChange}
+                />
+              </div>
+
+              <div class="input-group">
+                <span class="input-group-text">Comment</span>
+                <textarea class="form-control" rows="5"
+                  aria-label="With textarea"
+                  placeholder="Leave a comment here!"
+                  name="comment"
+                  value={comment}
+                  onChange={handleInputChange}
+                ></textarea>
+              </div>
+
+            </form>
 
           </div>
         </div>
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -9,6 +9,7 @@ export const useForm = (initialState = {}) => {
   }
 
   const handleInputChange = ({ target }) => {
+    console.log(target.value);
     setValues({
       ...values,
       [target.name]: target.value
