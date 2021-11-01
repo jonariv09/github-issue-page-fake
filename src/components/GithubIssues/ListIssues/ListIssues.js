2326fefd0d182ddabb05484008a4d9b82ed92150
Agustin <agustin.narvaez@contollo.net>
2021-11-01 17:33:03 -0600

    Adding style to list of issues

--- src/components/GithubIssues/ListIssues/ListIssues.js
+++ src/components/GithubIssues/ListIssues/ListIssues.js
@@ -1,32 +1,5 @@
-// import { loadIssues } from 'actions/issues';
-// import React, { useEffect } from 'react'
-// import { useDispatch, useSelector } from 'react-redux';
-// import { Issue } from '../Issue/Issue';
-
-// export const ListIssues = () => {
-
-//   const { issues } = useSelector((state) => state.issues);
-//   const dispatch = useDispatch()
-//   let notes = []
-
-//   useEffect(() => {
-    
-//     // consulta a base de datos
-    
-
-//     dispatch(loadIssues(notes));
-//   }, []);
-
-//   return (
-//     <>
-//       {
-//         notes &&
-//         notes.map(note => <Issue { ...note }/>) 
-//       }
-//     </>
-//   )
-// }
-
+import { FiStopCircle, FiCheck } from "react-icons/fi"
+import './ListIssues.css';
 
 /* This example requires Tailwind CSS v2.0+ */
 const people = [
@@ -45,19 +18,57 @@ const people = [
 export const ListIssues = () => {
   return (
     <div className="">
-      <div className="">
+      <div className="container">
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
           <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-            <table className="min-w-full divide-y divide-gray-200">
+            <table className="min-w-full divide-y divide-gray-200 table-issues">
               <thead className="bg-gray-50">
-                <tr>
+                <tr className="table-row-issues">
                   <th
                     scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500
+                      uppercase col-check
+                      d-flex align-items-center"
                   >
-                    Name
+                    <input
+                      id=""
+                      name=""
+                      type="checkbox"
+                      className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded me-2"
+                    />
+                    
+                    <a href="#">
+                      <FiStopCircle className="icon-issue ms-2"/>
+                    </a>
+
+                    <span className="ms-2"> { 2 } Open </span>
+
+                    <a href="#" className="d-flex align-items-center">
+                      <FiCheck className="ms-2"/>
+                      <span className="ms-1"> {0} Closed </span>
+                    </a>
+
+                    <ul className="ms-auto">
+                      <span>  </span>
+                    </ul>
+
                   </th>
+                  
+                  {/* <th
+                    scope="col"
+                    className="font-medium text-gray-500
+                      uppercase"
+                  >
+                  </th>
+
                   <th
+                    scope="col"
+                    className="font-medium text-gray-500
+                      uppercase tracking-wider"
+                  >
+                    
+                  </th> */}
+                  {/* <th
                     scope="col"
                     className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                   >
@@ -74,29 +85,36 @@ export const ListIssues = () => {
                     className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                   >
                     Role
-                  </th>
-                  <th scope="col" className="relative px-6 py-3">
+                  </th> */}
+                  {/* <th scope="col" className="relative px-6 py-3">
                     <span className="sr-only">Edit</span>
-                  </th>
+                  </th> */}
                 </tr>
               </thead>
               <tbody className="bg-white divide-y divide-gray-200">
                 {people.map((person) => (
                   <tr key={person.email}>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      <div className="flex items-center">
-                        <div className="flex-shrink-0 h-10 w-10">
-                          <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
+
+                    <td className="px-6 py-4 whitespace-nowrap d-flex align-items-start">
+                      <input
+                          id=""
+                          name=""
+                          type="checkbox"
+                          className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded me-2"
+                        />
+
+                        <FiStopCircle className="icon-issue-item ms-2 text-green-500"/>
+
+                        <div className="d-flex flex-column ms-3">
+                          <span className="title text-base font-bold"> This is my title </span>
+                          <span className="text-description text-xs"> This is my description </span>
                         </div>
-                        <div className="ml-4">
-                          <div className="text-sm font-medium text-gray-900">{person.name}</div>
-                          <div className="text-sm text-gray-500">{person.email}</div>
-                        </div>
-                      </div>
+
+                    </td>
+                    {/* <td className="px-6 py-4 whitespace-nowrap">
+                      
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap">
-                      <div className="text-sm text-gray-900">{person.title}</div>
-                      <div className="text-sm text-gray-500">{person.department}</div>
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap">
                       <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
@@ -108,7 +126,7 @@ export const ListIssues = () => {
                       <a href="#" className="text-indigo-600 hover:text-indigo-900">
                         Edit
                       </a>
-                    </td>
+                    </td> */}
                   </tr>
                 ))}
               </tbody>
